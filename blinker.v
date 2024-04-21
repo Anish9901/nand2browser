6e61966f78ba6c89f95c1180bcc4780c7572b1a7
Anish <umaleanish120@gmail.com>
2024-04-22 04:21:59 +0530

    fix indentation

--- blinker.v
+++ blinker.v
@@ -3,23 +3,23 @@
 module blinker(
     clk, led
 );
-	input clk;
-	output led;
-	//assign led = btn;
-	reg [23:0] ctr = 0;
-	reg l = 0;
-	parameter cmp = 24'd500000;
-	begin
-		always @(posedge clk)
-			begin
-				$display("Hello from blinker");
-				ctr <= ctr + 1;
-				if (ctr == cmp)
-					begin
-						ctr <= 0;
-						l <= ~l;
-					end
-			end
-		assign led = l;
-	end
+  input clk;
+  output led;
+  //assign led = btn;
+  reg [23:0] ctr = 0;
+  reg l = 0;
+  parameter cmp = 24'd500000;
+  begin
+    always @(posedge clk)
+      begin
+        $display("Hello from blinker");
+        ctr <= ctr + 1;
+        if (ctr == cmp)
+          begin
+            ctr <= 0;
+            l <= ~l;
+          end
+      end
+    assign led = l;
+  end
 endmodule
